a8dd5e5b1045dcb820e18ebc4ce131f4aad5af40
AronnePiperno <deferrari.alessandro@gmail.com>
2023-02-15 15:40:05 +0100

    project

--- Euler/euler.py
+++ Euler/euler.py
@@ -30,7 +30,53 @@ def problemsixtysix():
             
     
     print(f'{maxx} {maxD} {maxy} {minimum}')
-        
-                
 
-problemsixtysix()
+
+def ndigitreciprocals():
+    n = 10**7
+    ret = 0
+    for k in range(1,n+1):
+        value = 1/k
+        print(value)
+        try:
+            v = int(value[n])
+            print(v, k)
+        except:
+            v = 0
+        ret += v
+
+    print(ret)
+
+def SquaretheSmallest():
+    thedict = {}
+    L = [k for k in range(2,10**4)]
+    print(L)
+
+def Codedtrianglenumbers():
+    f = open('/home/ale/Backup/py/Euler/p042_words.txt', 'r')
+    words = f.read()
+    f.close
+    words = words.replace('"', '')
+    L = words.split(',')
+
+    generalcounter = 0
+
+    trianglenumbers = [n*(n+1)/2 for n in range(1000)]
+    
+    for word in L:
+        forsum = 0
+        for letter in word:
+            forsum += ord(letter)- ord('A')+1
+        if forsum in trianglenumbers:
+            generalcounter += 1
+        print(forsum)
+
+    print(generalcounter)
+
+
+#import os
+#
+#cwd = os.getcwd()  # Get the current working directory (cwd)
+#files = os.listdir(cwd)  # Get all the files in that directory
+#print("Files in %r: %s" % (cwd, files))
+Codedtrianglenumbers()
