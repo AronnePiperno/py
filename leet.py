b7948e7ca67015f5908def63083a5fbfb1589959
Ale <deferrari.alessandro@gmail.com>
2023-02-18 12:18:15 +0100

    com

---
+++ leet.py
@@ -0,0 +1,15 @@
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def minDiffInBST(self, root: TreeNode) -> int:
+        from collections import deque
+        deq = deque()
+
+
+Tree = TreeNode(1)
+
